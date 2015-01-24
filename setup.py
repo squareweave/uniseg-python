7dfe4d734d6b90238043dcf0b971af09beda338a
Masaaki Shibata <mshibata@emptypage.jp>
2015-01-24 22:21:12 +0900

    Added Python 3.4 classifier.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Text Processing',
     ],
 )
