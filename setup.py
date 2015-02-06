7a6da3eb4b8d96e75987ec10716dae2deeb7133b
Masaaki Shibata <mshibata@emptypage.jp>
2015-02-07 06:39:44 +0900

    Improve setup metadata.

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     author = 'Masaaki Shibata',
     author_email = 'mshibata@emptypage.jp',
     url = 'https://bitbucket.org/emptypage/uniseg-python',
-    description = 'A pure Python module to determine Unicode text segmentations',
+    description\
+        = 'A pure Python library to determine Unicode text segmentations',
+    long_description = open('README.txt').read(),
+    license = 'MIT',
     packages = ['uniseg'],
     package_data = {
         'uniseg': ['ucd.sqlite3', 'docs/*.html', 'samples/*.py']
@@ -22,7 +25,9 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Text Processing',
