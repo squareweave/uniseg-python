f77a7c9e76ccd9e7f5928d79aa4294e615bf1dc6
Masaaki Shibata <mshibata@emptypage.jp>
2015-02-07 01:09:47 +0900

    Move sample scripts under the module's 'samples' subdirectory.

--- setup.py
+++ setup.py
@@ -13,12 +13,11 @@ setup(
     description = 'A pure Python module to determine Unicode text segmentations',
     packages = ['uniseg'],
     package_data = {
-        'uniseg': ['ucd.sqlite3']
+        'uniseg': ['ucd.sqlite3', 'samples/*.py']
     },
     entry_points = {
         'console_scripts': ['uniseg-dbpath = uniseg.db:print_dbpath'],
     },
-    scripts = ['unibreak.py', 'uniwrap.py', 'wxwrapdemo.py'],
     classifiers = [
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
